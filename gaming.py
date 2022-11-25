14dc45e24329454bca2390ca9a611364b8ddedfb
KriShauna Fisher <Krishaunafisher@gmail.com>
2022-11-25 15:59:46 -0500

    displayed timer and restart functionality

--- gaming.py
+++ gaming.py
@@ -1,18 +1,27 @@
 import pygame
 from sys import exit
 
+
+def display_score():
+    current_time = int(pygame.time.get_ticks() / 1000) - start_time
+    score_surf = test_font.render(f"{current_time}", False, (64, 64, 64))
+    score_rect = score_surf.get_rect(center=(400, 50))
+    screen.blit(score_surf, score_rect)
+
+
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption("Run Run")
 clock = pygame.time.Clock()
 test_font = pygame.font.Font("platformerGraphicsDeluxe_Updated/Pixeltype.ttf", 50)
 game_active = True
+start_time = 0
 
 sky_surface = pygame.image.load("platformerGraphics_mushroomLand/Backgrounds/bg_grasslands.png").convert()
 ground_surface = pygame.image.load("platformerGraphicsDeluxe_Updated/ground.png").convert()
 
-score_surf = test_font.render("Run Run", False, (64, 64, 64))
-score_rect = score_surf.get_rect(center=(400, 50))
+# score_surf = test_font.render("Run Run", False, (64, 64, 64))
+# score_rect = score_surf.get_rect(center=(400, 50))
 
 snail_surf = pygame.image.load("platformerGraphicsDeluxe_Updated/snailWalk1.png").convert_alpha()
 snail_rect = snail_surf.get_rect(bottomright=(600, 300))
@@ -38,16 +47,19 @@ while True:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 game_active = True
                 snail_rect.left = 800
+                start_time = int(pygame.time.get_ticks() / 1000) - start_time
 
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
-    pygame.draw.rect(screen, "#c0e8ec",  score_rect)
-    pygame.draw.rect(screen, "#c0e8ec", score_rect, 10)
-    screen.blit(score_surf, score_rect)
+    # pygame.draw.rect(screen, "#c0e8ec",  score_rect)
+    # pygame.draw.rect(screen, "#c0e8ec", score_rect, 10)
+    # screen.blit(score_surf, score_rect)
+    # display_score()
+    display_score()
 
     if game_active:
         # snail
-        snail_rect.x -= 4
+        snail_rect.x -= 6
         if snail_rect.x <= 0:
             snail_rect.left = 800
         screen.blit(snail_surf, snail_rect)
